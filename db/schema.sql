810edd2df05a2a354b664375fb9765374b49b67b
OceanHelloWorld <oceanzyhy@gmail.com>
2019-05-14 09:42:28 -0700

    set up db

--- db/schema.sql
+++ db/schema.sql
@@ -2,22 +2,65 @@
  *    brew services start mysql
  *    mysql.server start
  *    mysql -u root < schema.sql
+ *    mysql -u root
  *  to create the database and the tables.*/
 
 
--- create a database for grocery_list app only if it doesn't exist
-CREATE DATABASE IF NOT EXISTS grocery_list;
+-- create a database for gist_list app only if it doesn't exist
+DROP DATABASE IF EXISTS gist_list;
+CREATE DATABASE IF NOT EXISTS gist_list;
 
-USE grocery_list;
+USE gist_list;
 
-CREATE TABLE IF NOT EXISTS grocery_items (
+CREATE TABLE IF NOT EXISTS users (
   id INT(10) NOT NULL auto_increment,
-  name VARCHAR(20) NOT NULL,
-  quantity SMALLINT(5) NOT NULL,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(20) NOT NULL,
   PRIMARY KEY(id)
 );
 
--- INSERT INTO grocery_items (name, quantity) VALUES ('apples', 8);
+CREATE TABLE IF NOT EXISTS gist_edits (
+  id INT(10) NOT NULL auto_increment,
+  timestamp SMALLINT(5) NOT NULL,
+  star SMALLINT(5) NOT NULL,
+  username INT(10),
+  FOREIGN KEY (username)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  url VARCHAR(20) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS files (
+  id INT(10) NOT NULL auto_increment,
+  intro VARCHAR(200),
+  file_name VARCHAR(20) NOT NULL,
+  content VARCHAR(2000),
+  gist_edit INT(10),
+  FOREIGN KEY (gist_edit)
+    REFERENCES gist_edits(id)
+    ON DELETE CASCADE,
+  PRIMARY KEY(id)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS gists_forked (
+  id INT(10) NOT NULL auto_increment,
+  gist_edit INT(10),
+  FOREIGN KEY (gist_edit)
+    REFERENCES gist_edits(id)
+    ON DELETE CASCADE,
+  username INT(10),
+  FOREIGN KEY (username)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  PRIMARY KEY(id)
+);
+
+
+-- INSERT INTO users (username, password) VALUES ('admin', 'p4ssw0rd');
 
 -- create a table to store items
 
